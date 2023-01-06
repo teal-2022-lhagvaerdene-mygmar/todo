760e4e0fdabf6e66d10005d367788bf5b2b3f0b7
teal-2022-lhagvaerdene-mygmar <teal.2022.lhagvaerdene.mygmar@gmail.com>
2023-01-06 17:22:17 +0800

    01-06

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+// const todo = ["hi", "za"];
 function App() {
+  const [text, setText] = useState("");
+  const [todos, setTodo] = useState([]);
+  const [error, setError] = useState("");
+  function textchange(e) {
+    setText(e.target.value);
+  }
+  function addTodo() {
+    if (text === "") {
+      setError("utgaa bicne uu");
+    } else {
+      const newTodo = [...todos, text];
+      setTodo(newTodo);
+      setText("");
+      setError("");
+    }
+  }
+  function deleteLi(bairlal) {
+    if (window.confirm("ustgah uu")) {
+      const newTodo = [...todos];
+      newTodo.splice(bairlal, 1);
+      setTodo(newTodo);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={text} onChange={textchange} />
+      <button onClick={addTodo}>nemeh</button>
+      {error && <div style={{ color: "red" }}> aldaa : {error}</div>}
+      <ul>
+        {todos.map((todo1, index) => (
+          <li key={index}>
+            {todo1}
+            <button onClick={() => deleteLi(index)}>delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
